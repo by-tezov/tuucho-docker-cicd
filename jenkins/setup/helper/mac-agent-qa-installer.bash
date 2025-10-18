5038755748f08446c9d58024f43823028684c9c2
tezov <tezov.app@gmail.com>
2025-10-18 09:29:41 +0200

    update installer

--- jenkins/setup/helper/mac-agent-qa-installer.bash
+++ jenkins/setup/helper/mac-agent-qa-installer.bash
@@ -3,76 +3,53 @@ set -e
 
 #CICD_FOLDER need to add this with ssh environment
 #sudo DevToolsSecurity -enable Needed on Mac to allow dev tool
+BUILDER_HOME=${HOME}/${CICD_FOLDER}/qa
 
-BUILDER_HOME="${HOME}/${CICD_FOLDER}/qa"
-_BREW_HOME="${BUILDER_HOME}/library/brew"
-_NODE_HOME="${BUILDER_HOME}/library/node"
+_JAVA_HOME=${BUILDER_HOME}/library/java
+JAVA_VERSION=17
+JAVA_JDK_URL=https://download.java.net/java/GA/jdk${JAVA_VERSION}.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-${JAVA_VERSION}.0.2_macos-aarch64_bin.tar.gz
 
-BREW_VERSION="4.6.7"
-JAVA_VERSION="17"
-NODE_VERSION="22.18.0"
+_NODE_HOME=${BUILDER_HOME}/library/node
+NODE_VERSION=22.18.0
+NODE_URL=https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-darwin-arm64.tar.gz
 
-APPIUM_VERSION="2.11.3"
-APPIUM_DRIVER_VERSION="9.10.5"
+APPIUM_VERSION=2.11.3
+APPIUM_DRIVER_VERSION=9.10.5
 
-ZSHRC="${BUILDER_HOME}/.zshrc"
+_BREW_HOME=${BUILDER_HOME}/library/brew
+BREW_VERSION=4.6.7
+BREW_GIT_URL=https://github.com/Homebrew/brew.git
+
+ZSHRC=${BUILDER_HOME}/.zshrc
 [ -f "${ZSHRC}" ] && rm "${ZSHRC}"
 touch "${ZSHRC}"
 
-# Homebrew
-if [ ! -d "${_BREW_HOME}" ]; then
-    echo "Installing Homebrew..."
-    mkdir -p "${_BREW_HOME}"
-    git clone --depth 1 https://github.com/Homebrew/brew.git "${_BREW_HOME}"
-    git -C "${_BREW_HOME}" fetch --tags --depth 1 --quiet
-    git -C "${_BREW_HOME}" -c advice.detachedHead=false checkout "refs/tags/${BREW_VERSION}"
-fi
-cat >> "${ZSHRC}" <<EOF
-BREW_HOME=${_BREW_HOME}
-NONINTERACTIVE=1
-HOMEBREW_PREFIX=\${BREW_HOME}
-HOMEBREW_CELLAR=\${BREW_HOME}/Cellar
-HOMEBREW_REPOSITORY=\${BREW_HOME}/Library/Homebrew
-HOMEBREW_CACHE=\${BREW_HOME}/cache
-HOMEBREW_TEMP=\${BREW_HOME}/tmp
-HOMEBREW_CASK_OPTS=\${BREW_HOME}/Applications
-HOMEBREW_NO_ENV_HINTS=1
-HOMEBREW_NO_AUTO_UPDATE=1
-HOMEBREW_DEVELOPER=1
-PATH=\${BREW_HOME}/bin:\${BREW_HOME}/sbin:\$PATH
-EOF
-source "${ZSHRC}"
-brew --version
-
 # Java
-if ! brew list --formula | grep -q "^openjdk@${JAVA_VERSION}\$"; then
-    echo "Installing OpenJDK ${JAVA_VERSION}"
-    brew install openjdk@${JAVA_VERSION} || true
+if [ ! -d "${_JAVA_HOME}/Contents" ]; then
+    echo "Installing Java from ${JAVA_JDK_URL}"
+    mkdir -p "${_JAVA_HOME}"
+    TMPDIR=$(mktemp -d)
+    curl -fsSL "${JAVA_JDK_URL}" -o "${TMPDIR}/openjdk.tar.gz"
+    tar -xzf "${TMPDIR}/openjdk.tar.gz" -C "${TMPDIR}"
+    mv "${TMPDIR}"/jdk-17*/* "${_JAVA_HOME}/"
+    rm -rf "${TMPDIR}"
 fi
 
-_JAVA_HOME="$(brew --prefix openjdk@${JAVA_VERSION})"
 cat >> "${ZSHRC}" <<EOF
-JAVA_HOME=${_JAVA_HOME}
+JAVA_HOME=${_JAVA_HOME}/Contents/Home
 PATH=\${JAVA_HOME}/bin:\$PATH
 EOF
 source "${ZSHRC}"
+java --version
 
-# Node.js
-if [ ! -d "${_NODE_HOME}" ]; then
-    echo "Installing NodeJs ${NODE_VERSION}"
+# NodeJs
+if [ ! -d "${_NODE_HOME}/bin" ]; then
+    echo "Installing NodeJs from ${NODE_URL}"
     mkdir -p "${_NODE_HOME}"
-    ARCH=$(uname -m)
-    if [ "$ARCH" = "arm64" ]; then
-        NODE_DIST="node-v${NODE_VERSION}-darwin-arm64"
-    else
-        NODE_DIST="node-v${NODE_VERSION}-darwin-x64"
-    fi
     TMPDIR=$(mktemp -d)
-        echo "https://nodejs.org/dist/v${NODE_VERSION}/${NODE_DIST}.tar.gz"
-
-    curl -fsSL "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-darwin-arm64.tar.gz" -o "${TMPDIR}/node.tar.gz"
+    curl -fsSL "${NODE_URL}" -o "${TMPDIR}/node.tar.gz"
     tar -xzf "${TMPDIR}/node.tar.gz" -C "${TMPDIR}"
-    mv "${TMPDIR}/${NODE_DIST}"/* "${_NODE_HOME}/"
+    mv "${TMPDIR}/node-"*/* "${_NODE_HOME}/"
     rm -rf "${TMPDIR}"
 fi
 
@@ -110,6 +87,31 @@ elif [[ "$CURRENT_APPIUM_DRIVER_VERSION" != "$APPIUM_DRIVER_VERSION" ]]; then
 fi
 echo "appium-driver $(appium driver list --installed --json | jq -r '.xcuitest.version // empty')"
 
+# Homebrew
+if [ ! -d "${_BREW_HOME}/bin" ]; then
+    echo "Installing Homebrew..."
+    mkdir -p "${_BREW_HOME}"
+    git clone --depth 1 "${BREW_GIT_URL}" "${_BREW_HOME}"
+    git -C "${_BREW_HOME}" fetch --tags --depth 1 --quiet
+    git -C "${_BREW_HOME}" -c advice.detachedHead=false checkout "refs/tags/${BREW_VERSION}"
+fi
+cat >> "${ZSHRC}" <<EOF
+BREW_HOME=${_BREW_HOME}
+NONINTERACTIVE=1
+HOMEBREW_PREFIX=\${BREW_HOME}
+HOMEBREW_CELLAR=\${BREW_HOME}/Cellar
+HOMEBREW_REPOSITORY=\${BREW_HOME}/Library/Homebrew
+HOMEBREW_CACHE=\${BREW_HOME}/cache
+HOMEBREW_TEMP=\${BREW_HOME}/tmp
+HOMEBREW_CASK_OPTS=\${BREW_HOME}/Applications
+HOMEBREW_NO_ENV_HINTS=1
+HOMEBREW_NO_AUTO_UPDATE=1
+HOMEBREW_DEVELOPER=1
+PATH=\${BREW_HOME}/bin:\${BREW_HOME}/sbin:\$PATH
+EOF
+source "${ZSHRC}"
+brew --version
+
 # Appium required
 if ! brew tap | grep -q "^wix/brew\$"; then
     echo "Tapping wix/brew..."
@@ -118,19 +120,19 @@ fi
 
 if ! command -v applesimutils &>/dev/null; then
     echo "Installing applesimutils..."
-    brew install applesimutils
+    brew install --build-from-source applesimutils
 fi
 applesimutils --version
 
 if ! command -v ios-deploy &>/dev/null; then
     echo "Installing ios-deploy..."
-    brew install ios-deploy
+    brew install --build-from-source ios-deploy
 fi
 echo "ios-deploy $(ios-deploy --version)"
 
 if ! command -v set-simulator-location &>/dev/null; then
     echo "Installing set-simulator-location..."
-    brew install lyft/formulae/set-simulator-location
+    brew install --build-from-source lyft/formulae/set-simulator-location
 fi
 echo "set-simulator-location $(command -v set-simulator-location &>/dev/null && echo "ok")"
 
